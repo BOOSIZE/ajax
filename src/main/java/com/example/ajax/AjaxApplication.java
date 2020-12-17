12b79a2fff3a88fe4d115d1fdd4673fbcfdde067
29778 <2977898490@qq.com>
2020-12-17 13:49:18 +0800

    第一次

---
+++ src/main/java/com/example/ajax/AjaxApplication.java
@@ -0,0 +1,16 @@
+package com.example.ajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(AjaxApplication.class, args);
+		System.out.println("服务器已启动");
+	}
+
+}
